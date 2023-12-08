f6bd7b33c8b74faae30ad4dca6d0afa105464c6f
Totoril <totoril@Swifty.com>
2023-12-08 17:53:43 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

---
+++ fetch_municipalities.sh
@@ -0,0 +1,29 @@
+t#!/bin/bash
+
+#Fetching the Wkik page
+url="https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway"
+curl -s "$url" > wikipedia_page.html 
+
+#Extracting the table with grep (find the right wikipage structure)
+grep -oP 'table class="wikitable".*?</table>' wikipedia_page.html > extracted_table.html
+
+cat <<EOL > output.html
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+                <meta charset="UTF-8" />
+                <title>Candidate 10028's Project Page</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <meta name="description" content="" />
+        </head>
+        <body>
+                <h1>This is my page for the Individual Project</h1>
+                <p>This is a simple webpage served by Apache on Ubuntu.</p>
+
+$(cat extracted_table.html)
+        </body>
+        </html>
+        EOL
+
+xdg-oben output.html
+        
