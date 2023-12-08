cd258e700c64f7fdf9b63ab52140b022cfc6729c
Totoril <totoril@Swifty.com>
2023-12-08 15:30:27 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

--- html.sh
+++ html.sh
@@ -2,7 +2,7 @@
 
 # requests a page, follwing redirects, and preps it for sed processing
 function get_page(){
-    local projectpage_toril_hokseggen/index1.html.txt
+    local projectpage_toril_hokseggen
     url=$1
     fname=$2
 
