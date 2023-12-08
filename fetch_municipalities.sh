7f84ac77e342e7afb166cf3a6244f4ee70bb52dc
Totoril <totoril@Swifty.com>
2023-12-08 20:46:08 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

--- fetch_municipalities.sh
+++ fetch_municipalities.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 
 #Fetching the Wkik page
-url="https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway"
-curl -s "$url" > wikipedia_page.html
+wget -O wikipedia_page.html https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway
+
 
 #Extracting the table with grep (find the right wikipage structure)
-awk '/<table class="sortable.wikitable.jquery-tablesorter"/,/</table>/' wikipedia_page.html > extracted_table.html
-
+#awk '/<table class="sortable.wikitable.jquery-tablesorter"/,/</table>/' wikipedia_page.html > extracted_table.html
+cat wikipedia_page.html | grep -A 100 '<table' | grep -B 100 '</table>' > extracted_table.html
 #Filtering the extrated table with awk
-awk '/<table/,/<\/table>/' extracted_table.html > filtered_table.html
+#awk '/<table/,/<\/table>/' extracted_table.html > filtered_table.html
 
-cat > output.html <<EOL
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-                <meta charset="UTF-8" />
-                <title>Candidate 10028's Project Page</title>
-                <meta name="viewport" content="width=device-width,initial-scale=1" />
-                <meta name="description" content="" />
-        </head>
-        <body>
-                <h1>This is my page for the Individual Project</h1>
-                <p>This is a simple webpage served by Apache on Ubuntu.</p>
+#cat > output.html <<EOL
+ #       <!DOCTYPE html>
+  #      <html lang="en">
+   #     <head>
+    #            <meta charset="UTF-8" />
+     #           <title>Candidate 10028's Project Page</title>
+      #          <meta name="viewport" content="width=device-width,initial-scale=1" />
+       #         <meta name="description" content="" />
+        #</head>
+        #<body>
+         #       <h1>This is my page for the Individual Project</h1>
+          #      <p>This is a simple webpage served by Apache on Ubuntu.</p>
 
-$(cat filtered_table.html)
-</body>
-</html>
-EOL
+#$(cat filtered_table.html)
+#</body>
+#</html>
+#EOL
 
-xdg-open output.html
-
+#xdg-open output.html
