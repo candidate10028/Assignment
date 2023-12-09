850cbfa55398a827c3263cdb60dcf1266067a4c3
Totoril <totoril@Swifty.com>
2023-12-09 16:43:40 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

--- fetch_municipalities.sh
+++ fetch_municipalities.sh
@@ -5,9 +5,8 @@
 curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > "wikipedia_page.html.txt"
 
 cat "wikipedia_page.html.txt" | tr -d '\n\t' > "wikipage.html.one.line.txt"
-sed -E 's|(<table class"sortable wikitable jquery-tablesorter">)|\n
-\1\g' "wikipage.html.on.line.txt" > "extracted_table.html"
-sed -E 's|(</table>)|\1\n|g' "page.html.one.line.txt" > "page.html.table.newline.txt"
+
+sed -E 's/.<table class"sortable wikitable jquery-tablesorter">(.*)</table>.*/\1\g' wikipage.html.on.line.txt | sed 's/</table>/\1/g' | sed -n '1p' | grep -o '<tbody[ >].*<\/tbody' |sed -E 's/<tbody[^>]*>(.*)<\/tbody>/\1/g' | sed -E 's/<tr[^>]*>//g' | sed 's/<\/tr>/\n/g' | sed -E 's/<td[^>]*>//g' | sed 's/<\/td>/\t/g' | sed '1d' > "extracted_table.html.txt"
 
 #Extracting the table with grep (find the right wikipage structure)
 #awk '/<table class="sortable.wikitable.jquery-tablesorter"/,/</table>/' wikipedia_page.html > extracted_table.html
