c2a667756f9b6e5409e798b5765278165ef63f61
Totoril <totoril@Swifty.com>
2023-12-09 17:37:20 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

--- fetch_municipalities.sh
+++ fetch_municipalities.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-# Download the Wikipedia page
-curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > "wikipedia_page.html.txt"
+# 1 - save the index webpage as an HTML file
+curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > wiki.list.html
 
-# Combine the content into one line
-cat "wikipedia_page.html.txt" | tr -d '\n\t' > "wikipage.html.one.line.txt"
+# 2 - remove all newlines and tabs; prepare the file for grep (because grep evaluates everything line by line, so we'd want all HTML/text on a single line)
+cat "wiki.list.html" | tr -d '\n\t' > wiki.list.no.newlines.html
 
-# Extract content between <table> and </table> tags
-sed -n -E 's|<table class="sortable wikitable jquery-tablesorter">(.*)</table>|\1|p' "wikipage.html.one.line.txt" > "test.txt"
-
-# Remove </table> tags
-sed -E 's|<\/table>||g' "test.txt" > "test_tmp.txt" && mv "test_tmp.txt" "test.txt"
-
-# Uncomment the following lines if you want to perform additional processing
-sed -n '1p' "test.txt"
-
-grep -o '<tbody[ >].*<\/tbody' "test.txt" | sed -E 's|<tbody[^>]*>(.*)<\/tbody>|\1|g'
-
-sed -E 's|<tr[^>]*>|\n|g' "test.txt" | sed '/^$/d'
-
-sed -E 's|<td[^>]*>|\t|g' "test.txt" | sed -E 's|<\/td>||g' > "extracted_table.html.txt"
+# 3 - find the table with all links; extract it; the table has classes "sortable wikitable", which are used to identify it; <table> and <tbody> are removed; <tr> are replaced with newline characters; <td> are replaced with "tab" characters; the 1st line is deleted
+sed -E 's/.*<table class="sortable wikitable">(.*)<\/table>.*/\1/g' wiki.list.no.newlines.html | sed 's/<\/table>/\n/g' | sed -n '1p' | grep -o '<tbody[ >].*<\/tbody>' | sed -E 's/<tbody[^>]*>(.*)<\/tbody>/\1/g' | sed -E 's/<tr[^>]*>//g' | sed 's/<\/tr>/\n/g' | sed -E 's/<td[^>]*>//g' | sed 's/<\/td>/\t/g' | sed '1d' > table.txt
