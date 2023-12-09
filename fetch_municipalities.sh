8431fd3015118c0f84ed06872567ff77aa87bf76
Totoril <totoril@Swifty.com>
2023-12-09 17:04:47 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

--- fetch_municipalities.sh
+++ fetch_municipalities.sh
@@ -3,8 +3,8 @@ curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > "wikipe
 
 cat "wikipedia_page.html.txt" | tr -d '\n\t' > "wikipage.html.one.line.txt"
 
-sed -E 's|<table class="sortable wikitable jquery-tablesorter">(.*)</table>|\1|g' wikipage.html.one.line.txt | sed 's/</table>/\1/g' | sed -n '1p' 
-#| grep -o '<tbody[ >].*<\/tbody' 
+sed -E 's|<table class="sortable wikitable jquery-tablesorter">(.*)</table>|\1|g' wikipage.html.one.line.txt > test.txt
+#| sed 's/</table>/\1/g' | sed -n '1p' | grep -o '<tbody[ >].*<\/tbody' 
 #| sed -E 's/<tbody[^>]*>(.*)<\/tbody>/\1/g' 
 #| sed -E 's/<tr[^>]*>//g' 
 #| sed 's/<\/tr>/\n/g' 
