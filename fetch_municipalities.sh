9226905f98b0e2be417ccb9eac376114ba7fe851
Totoril <totoril@Swifty.com>
2023-12-09 17:19:29 +0100

    git remote add origin https://github.com/totoril/Assignment.git
        git branch -M main
        git push -u origin main

--- fetch_municipalities.sh
+++ fetch_municipalities.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+
+# Download the Wikipedia page
 curl -s https://en.wikipedia.org/wiki/List_of_municipalities_of_Norway > "wikipedia_page.html.txt"
 
+# Combine the content into one line
 cat "wikipedia_page.html.txt" | tr -d '\n\t' > "wikipage.html.one.line.txt"
 
-sed -E 's|<table class="sortable wikitable jquery-tablesorter">(.*)</table>|\1|g' wikipage.html.one.line.txt > test.txt
-#| sed 's/</table>/\1/g' | sed -n '1p' | grep -o '<tbody[ >].*<\/tbody' 
-#| sed -E 's/<tbody[^>]*>(.*)<\/tbody>/\1/g' 
-#| sed -E 's/<tr[^>]*>//g' 
-#| sed 's/<\/tr>/\n/g' 
-#| sed -E 's/<td[^>]*>//g' 
-#| sed 's/<\/td>/\t/g' 
-#| sed '1d' > "extracted_table.html.txt"
+# Extract content between <table> and </table> tags
+sed -n -E 's|<table class="sortable wikitable jquery-tablesorter">(.*)</table>|\1|p' "wikipage.html.one.line.txt" > "test.txt"
+
+# Remove </table> tags
+sed -E 's|<\/table>||g' "test.txt" > "test_tmp.txt" && mv "test_tmp.txt" "test.txt"
+
+# Uncomment the following lines if you want to perform additional processing
+sed -n '1p' "test.txt"
+
+grep -o '<tbody[ >].*<\/tbody' "test.txt" | sed -E 's|<tbody[^>]*>(.*)<\/tbody>|\1|g'
+
+sed -E 's|<tr[^>]*>|\n|g' "test.txt" | sed '/^$/d'
+
+sed -E 's|<td[^>]*>|\t|g' "test.txt" | sed -E 's|<\/td>||g' > "extracted_table.html.txt" > test.txt
